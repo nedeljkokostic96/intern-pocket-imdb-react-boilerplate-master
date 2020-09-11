674154fc2c6752a8fd46aba3e64423eaf8b6f6bf
Nedeljko Kostic <nedeljkokostic0@gmail.com>
2020-09-11 11:47:37 +0200

    Register validation added

--- src/containers/auth/Register.js
+++ src/containers/auth/Register.js
@@ -1,73 +1,89 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-
-import { register } from '../../store/actions/AuthActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { register } from "../../store/actions/AuthActions";
 
 class Register extends Component {
   state = {
-    email: '',
-    password: '',
-    name: ''
+    email: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
   };
 
-  handleInputChange = field => event => this.setState({ [field]: event.target.value });
+  handleInputChange = (field) => (event) =>
+    this.setState({ [field]: event.target.value });
 
-  submit = event => {
-    event.preventDefault();
-
+  submit = (form) => {
     let registerData = {
-      email: this.state.email,
-      password: this.state.password,
-      name: this.state.name
+      email: form.email,
+      password: form.password,
+      name: form.name,
     };
     this.props.register(registerData);
   };
 
+  signupSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email")
+      .max(255, "Too Long!")
+      .required("Required"),
+    password: Yup.string().min(6, "Too short").required("Required"),
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "Passwords must match"
+    ),
+    name: Yup.string()
+      .min(2, "Too Short!")
+      .max(255, "Too Long!")
+      .required("Required"),
+  });
+
   render() {
     return (
-      <div>
-        <form onSubmit={this.submit}>
-          <h2>Register</h2>
-          <input
-            type="text"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleInputChange('email')}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleInputChange('password')}
-          />
-          <input
-            type="text"
-            placeholder="Name"
-            value={this.state.name}
-            onChange={this.handleInputChange('name')}
-          />
-          <input type="submit" value="Register" />
-          {this.props.registerError && <p>registerError</p>}
-        </form>
-      </div>
+      <Formik
+        onSubmit={this.submit}
+        initialValues={this.state}
+        validationSchema={this.signupSchema}
+      >
+        {() => (
+          <Form>
+            <Field name="email" type="email" placeholder="Email" />
+            <ErrorMessage component="div" name="email" />
+            <br />
+            <Field name="password" type="password" placeholder="Password" />
+            <ErrorMessage component="div" name="password" />
+            <br />
+            <Field
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirm password"
+            />
+            <ErrorMessage component="div" name="confirmPassword" />
+            <br />
+            <Field name="name" type="text" placeholder="Name" />
+            <ErrorMessage component="div" name="name" />
+            <br />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    registerError: state.error.registerError
+    registerError: state.error.registerError,
   };
 };
 
 const mapDispatchToProps = {
-  register
+  register,
 };
 
 export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Register)
+  connect(mapStateToProps, mapDispatchToProps)(Register)
 );
