172ffd24d477ef8e879818244e3588fc9545ae79
Nedeljko Kostic <nedeljkokostic0@gmail.com>
2020-09-11 09:49:19 +0200

    Enabling guests to see all movies

--- src/component/AppLayout.js
+++ src/component/AppLayout.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import Login from '../containers/auth/Login';
-import Register from '../containers/auth/Register';
-import Home from '../containers/Home';
-import { authUser } from '../store/actions/AuthActions';
+import Login from "../containers/auth/Login";
+import Register from "../containers/auth/Register";
+import Home from "../containers/Home";
+import { authUser } from "../store/actions/AuthActions";
 
 class AppLayout extends React.Component {
   componentDidUpdate(prevProps) {
     if (this.props.user !== prevProps.user) {
       if (this.props.user) {
-        this.props.history.push('/home');
+        this.props.history.push("/home");
       } else {
-        this.props.history.push('/login');
+        this.props.history.push("/login");
       }
     }
   }
 
   render() {
-    return this.props.user ? (
+    return (
       <div>
         <Route exact path="/home" component={Home} />
-      </div>
-    ) : (
-      <div>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
       </div>
@@ -33,21 +30,18 @@ class AppLayout extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    user: state.authUser
+    user: state.authUser,
   };
 };
 
 const mapDispatchToProps = () => {
   return {
-    authUser
+    authUser,
   };
 };
 
 export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(AppLayout)
+  connect(mapStateToProps, mapDispatchToProps)(AppLayout)
 );
